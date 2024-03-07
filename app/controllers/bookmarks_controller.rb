36965f11c0a47f56a13b79d5100dcea22a18a76b
Andrea Basili <andrea.basili@live.it>
2024-03-07 14:56:41 +0100

    define new action, form partial, connect to lists

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,8 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create, :destroy]
+
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
@@ -9,7 +12,11 @@ class BookmarksController < ApplicationController
   end
 
   private
-
   def bm_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
