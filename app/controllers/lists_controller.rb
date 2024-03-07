1e9f755a160c302475c23a533c611fc115567e56
Andrea Basili <andrea.basili@live.it>
2024-03-07 12:31:03 +0100

    defined show method with the private ones and its view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+
   def index
     @lists = List.all
   end
@@ -15,8 +17,10 @@ class ListsController < ApplicationController
   private
 
   def list_params
+    params.require(:list).permit(:name)
   end
 
   def set_list
+    @list = List.find(params[:id])
   end
 end
