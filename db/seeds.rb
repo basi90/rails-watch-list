8cd27d7e8df27f9564bd8049e4382d2af3424d2f
Andrea Basili <andrea.basili@live.it>
2024-03-08 16:52:42 +0100

    connection to TMDB API and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,23 @@
-require "faker"
+require "dotenv/load"
+require "json"
+require "open-uri"
 
 Movie.destroy_all
 
-10.times do
+tmdb_api = ENV["TMDB_API_KEY"]
+page_size = 10
+url = "https://api.themoviedb.org/3/movie/popular?api_key=#{tmdb_api}&language=en-US&page=1&per_page=#{page_size}"
+
+response = URI.open(url)
+result = JSON.parse(response.read)
+
+movies = result["results"]
+
+movies.each do |movie_data|
   Movie.create(
-    title: Faker::Movie.title,
-    overview: Faker::Lorem.paragraph(sentence_count: 3),
-    poster_url: Faker::Internet.url,
-    rating: Faker::Number.decimal(l_digits: 1, r_digits: 1)
+    title: movie_data["title"],
+    overview: movie_data["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie_data['poster_path']}",
+    rating: movie_data["vote_average"]
   )
 end
